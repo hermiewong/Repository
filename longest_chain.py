841c509cce450eed03b19a4be4ce22dfa5a6ce80
Herman Wong <hermanpad626@gmail.com>
2024-02-14 11:05:54 +0000

    optimise more

--- longest_chain.py
+++ longest_chain.py
@@ -1,4 +1,4 @@
-def link(relation): # assumes there is a point at top and bottom of interval, and relations is sorted in time order
+def link(relation,test=0): # assumes there is a point at top and bottom of interval, and relations is sorted in time order
     link={}
     #for bottom point we know top point will not be link so
     #we bypass this point with a separate loop
@@ -12,28 +12,53 @@ def link(relation): # assumes there is a point at top and bottom of interval, an
                 break
         if append==0:
             ith_link.append(value)
-
+    trim_relation=relation.copy()
+    trim_relation.pop(0)
+    trim_relation.pop(len(trim_relation))
     link[0]=ith_link
-    for i,j in enumerate(relation):#last element has no relations
-        if i==0 or i==len(relation)-1:
-            continue
-        candidate=relation[j]
-        ith_link=[candidate[0]]#closest in time with a relation is always a link
-        for index,value in enumerate(candidate[1:]):
-            append=0
-            for k in candidate[0:index+1]:
-                if k<j and value in link[k]:
-                    append+=1
-                    break
-                elif k>=j and value in relation[k]:
-                    append+=1
-                    break
-            if append==0:
-                ith_link.append(value)
-        link[j]=ith_link
+    if test==0:
+        for i,j in enumerate(trim_relation):#last element has no relations
+            candidate=relation[j]
+            ith_link=[candidate[0]]#closest in time with a relation is always a link
+            for index,value in enumerate(candidate[1:]):
+                append=0
+                for k in candidate[0:index+1]:
+                    if value in relation[k]:
+                        append+=1
+                        break
+                    # if k<j and value in link[k]:
+                    #     append+=1
+                    #     break
+                    # elif k>=j and value in relation[k]:
+                    #     append+=1
+                    #     break
+                if append==0:
+                    ith_link.append(value)
+            link[j]=ith_link
+    elif test==1:
+        for i,j in enumerate(trim_relation):#last element has no relations
+            candidate=relation[j]
+            ith_link=[candidate[0]]#closest in time with a relation is always a link
+            for index,value in enumerate(candidate[1:]):
+                append=0
+                for k in candidate[0:index+1]:
+                    # if value in relation[k]:
+                    #     append+=1
+                    #     break
+                    if k<j:
+                        if value in link[k]:
+                            append+=1
+                            break
+                    elif k>=j:
+                        if value in relation[k]:
+                            append+=1
+                            break
+                if append==0:
+                    ith_link.append(value)
+            link[j]=ith_link
     return link
 
-def longest_chain(link,test=0,cycle=20):
+def longest_chain(link,cycle=20):
     """
     link: dictionary of links
     """
@@ -41,84 +66,38 @@ def longest_chain(link,test=0,cycle=20):
     for i in link[0]:
         maximal.append([0,i])
     flow=1
-    if test==False:
-        while flow>0:
-            flow=0
+    count=0
+    while flow>0:
+        count+=1
+        flow=0
+        if count==cycle:
+            count=0
+            endpoint=[]
             maximal_copy=[]
             for i in maximal:
-                # if i=='remove':
-                #     continue
-                if link.get(i[-1])==None:
-                    continue
-                for j in link[i[-1]]:
-                    # for k in maximal:#check for longer route by removing shorter routes to the same point
-                    #     if j in k and not j==k[-1]:
-                    #         k='remove'
-                    element=i.copy()
-                    element.append(j)
-                    maximal_copy.append(element)
-                    flow+=1
-            if flow!=0:
-                maximal=maximal_copy
-    else:
-        count=0
-        while flow>0:
-            count+=1
-            flow=0
-            maximal_copy=[]
-            if count==cycle:
-                count+=1
-                for i in maximal:
-                    if link.get(i[-1])==None:
-                        continue
-                    for j in link[i[-1]]:
-                        for k in maximal:#check for longer route by removing shorter routes to the same point
-                            if j in k:
-                                k='remove'
-                        element=i.copy()
-                        element.append(j)
-                        maximal_copy.append(element)
-                        flow+=1
-                if flow!=0:
-                    maximal=maximal_copy
+                if not i[-1] in endpoint:
+                    endpoint.append(i[-1])
+                    maximal_copy.append(i)
+            maximal=maximal_copy
+        maximal_copy=[]
+        for i in maximal:
+            if link.get(i[-1])==None:
                 continue
-            
-            if count==cycle+1:
-                count=0
-                for i in maximal:
-                    if i=='remove':
-                        continue
-                    if link.get(i[-1])==None:
-                        continue
-                    for j in link[i[-1]]:
-                        element=i.copy()
-                        element.append(j)
-                        maximal_copy.append(element)
-                        flow+=1
-                if flow!=0:
-                    maximal=maximal_copy
-            else:
-                for i in maximal:
-                    if link.get(i[-1])==None:
-                        continue
-                    for j in link[i[-1]]:
-                        element=i.copy()
-                        element.append(j)
-                        maximal_copy.append(element)
-                        flow+=1
-                if flow!=0:
-                    maximal=maximal_copy
+            for j in link[i[-1]]:
+                element=i.copy()
+                element.append(j)
+                maximal_copy.append(element)
+                flow+=1
+        if flow!=0:
+            maximal=maximal_copy
     return maximal
             
-
-
-
 if __name__=='__main__':
     import ADS2 as ads
-    N=150
+    N=1500
     L=100
     d=2
-    cycle=100
+    cycle=1
     flat=ads.sprinkle(L,N,d)
     relation={}
     for i,base in enumerate(flat):
@@ -140,16 +119,14 @@ if __name__=='__main__':
     # print(relation)
     import time
     t0=time.time()
-    lonk=link(relation)
+    lonk=link(relation,0)
     t1=time.time()
-    # print(lonk)
-    long=longest_chain(lonk,0)
-    t2=time.time()
-    long=longest_chain(lonk,1,cycle)
+    lonk=link(relation,1)
     t3=time.time()
-    print('Link time=',t1-t0)
-    print('Original=',t2-t1)
-    print('Test=',t3-t2)
+    long=longest_chain(lonk,cycle)
+    t2=time.time()
+    print('Link time=',t1-t0,t3-t1)
+    print('longest chain time=',t2-t1)
     print("longest chains length:",len(long[0]))
     volume=L**2/2
     print('Number of points sprinkled:',len(flat))
